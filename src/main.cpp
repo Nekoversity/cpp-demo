19915db9dc4a58b55d980be2675047fe0093dbd1
ka1tzyu <ka1tzyu@gmail.com>
2022-02-13 13:54:55 +0200

    [UPD] Used SimpleCalculator::step_compute()

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,13 @@ using std::endl;
 
 int main() {
     calculator::SimpleCalculator calc(-0.71, 1.17);
-    cout << "RESULT: " << calc.compute(5.42) << endl;
+    cout << "compute() RESULT: " << calc.compute(5.42) << endl;
+
+    cout << endl << "===================" << endl << endl;
+
+    double results[10];
+    calc.step_compute(5.1, 6, results);
+    for (double result : results) {
+        cout << "step_compute() RESULT: " << result << endl;
+    }
 }
