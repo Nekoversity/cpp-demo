d20ffb30b10a4d9f3df414f101390451646bfeff
ka1tzyu <ka1tzyu@gmail.com>
2022-02-17 14:08:54 +0200

    [UPD] Pretty output for simple-calc-app

--- projects/simple_calc_app/src/SimpleCalcApp.cpp
+++ projects/simple_calc_app/src/SimpleCalcApp.cpp
@@ -6,13 +6,13 @@ using std::endl;
 
 int main() {
     simple::SimpleCalculator calc(-0.71, 1.17);
-    cout << "compute() RESULT: " << calc.compute(5.42) << endl;
+    cout << "> compute(5.42)  =  " << calc.compute(5.42) << endl;
 
-    cout << endl << "===================" << endl << endl;
+    cout << endl << endl;
 
     double results[10];
     calc.step_compute(5.1, 6, results);
     for (double result : results) {
-        cout << "step_compute() RESULT: " << result << endl;
+        cout << "> step_compute(5.1, 6)  =  " << result << endl;
     }
 }
